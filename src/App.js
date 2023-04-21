0ef5e292cfe70f49f6e257e0946f25cb0bd19d3d
Ahortu901 <aricktech.edu@gmail.com>
2023-04-21 00:26:38 +0000

    config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const App = () => {
+    return (
+      <ChatEngine
+        height="100vh"
+        projectID="b27134b4-8136-4bfa-9589-a244a39f69ae"
+        userName="Derrick"
+        userSecret="alxchat"
+      />
+    );
+  };
+  
+  // infinite scroll, logout, more customizations...
+  
+  export default App;
