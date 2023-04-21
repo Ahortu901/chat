515321f8973dd8d10ceaa1bc9bc43ee02646f0e9
Ahortu901 <aricktech.edu@gmail.com>
2023-04-21 01:28:19 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+
 import './App.css';
 
 const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
     return (
       <ChatEngine
         height="100vh"
         projectID="b27134b4-8136-4bfa-9589-a244a39f69ae"
-        userName="Derrick"
-        userSecret="alxchat"
+        userName={localStorage.getItem('Username')}
+        userSecret={localStorage.getItem('Password')}
         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
       />
     );
